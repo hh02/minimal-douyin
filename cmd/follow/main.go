f417a3f944ccd9a0afa4f754cfee1c5256cf0c85
huihao <huihao20@gmail.com>
2022-06-04 00:09:42 +0800

    fixed bug

--- cmd/follow/main.go
+++ cmd/follow/main.go
@@ -28,7 +28,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8888")
+	addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:8888")
 	if err != nil {
 		panic(err)
 	}
